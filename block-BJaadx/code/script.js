e474d218aa1894eff218c39b226179846ccd32a2
Gurinder121 <gurinderkhabra@gmail.com>
2021-11-26 11:36:28 -0800

    Incomplete

--- block-BJaadx/code/script.js
+++ block-BJaadx/code/script.js
@@ -1,5 +1,5 @@
 // 1. Change the title of the page to `Hello AltCampus!`
-
+document.getElementById("test") = "Penis";
 // 2. Select the element using the children property:
 
 //    - Select the `h1` element and change the value to `Learning DOM`
